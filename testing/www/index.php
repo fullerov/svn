1d080ad95a1243e261a160e3366b11b4de0b29df
Shapovalov A.A <wersev@mail.ru>
2013-05-17 22:08:03 -0700

    ~94% ready

---
+++ testing/www/index.php
@@ -0,0 +1,28 @@
+<? 
+session_start();
+try{
+//пути до файлов с классами	
+set_include_path(get_include_path()
+.PATH_SEPARATOR.'server/controllers'
+.PATH_SEPARATOR.'server/models'
+.PATH_SEPARATOR.'server/views');
+//автозагрузка объявленых классов
+function __autoload($name)
+{require_once $name.'.php';}
+//вызов класса-обработчика запросов
+$front=FrontController::get();
+$front->route();
+echo $front->getBody();
+}
+
+//при возникновении ошибки
+catch(Exception $e)
+{echo '<center><b>Ошибка в файле: '.$e->getFile().'<br>На строке '.$e->getLine().'<br>'.$e->getMessage().'</b></center>';}
+
+?>
+
+
+
+
+ 
+
